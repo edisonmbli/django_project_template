d94d31ac810bd5ba39e3e8109ad3b7e3f5b34950
edisonmbli <edisonmbli@gmail.com>
2025-05-13 23:03:04 +0800

    static asset & boostrap included

--- pages/tests.py
+++ pages/tests.py
@@ -2,9 +2,8 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from pages.views import HomePageView
 
+
 # Create your tests here.
-
-
 class HomePageTests(SimpleTestCase):
     def setUp(self):
         url = reverse("home")
@@ -28,4 +27,26 @@ class HomePageTests(SimpleTestCase):
 
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)  # noqa: E501
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template_name_correct(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_contains_correct_content(self):
+        self.assertContains(self.response, "About")
+
+    def test_not_contains_incorrect_content(self):
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")  # noqa: E501
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)  # noqa: E501
